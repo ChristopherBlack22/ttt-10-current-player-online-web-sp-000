7a55bc6da0536f55871bbb16db5aa7a70838166e
Christopher Black <christopher.black.2209@googlemail.com>
2021-03-22 21:50:29 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,10 +9,5 @@ def turn_count(board)
 end 
 
 def current_player(board)
-  #if 
-    turn_count(board).even? ? "X" : "O"
-    #return "X"
-  #else
-    #return "O"
-  #end
+  turn_count(board).even? ? "X" : "O"
 end
